24268b98442de2b9ffdaa407d4a0cb7e58d37e5b
Liong <axel.tanzil@student.citrakasih.sch.id>
2025-12-03 20:55:55 +0800

    ecolife

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import Profile from './pages/Profile';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import QRScan from "./pages/QRScan";
+import Tips from "./pages/Tips";
+import Profile from "./pages/Profile";
+import ProfileMenu from "./pages/ProfileMenu";
+import TipDetail from "./pages/TipDetail";
 
-function App() {
-  return <Profile />;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/scan" element={<QRScan />} />
+        <Route path="/tips" element={<Tips />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile-menu" element={<ProfileMenu />} />
+        <Route path="/tip/:id" element={<TipDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App;
